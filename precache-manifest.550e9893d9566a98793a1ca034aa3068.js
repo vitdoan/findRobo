6eb0cb505cb741cb2b49b08ef2c50bdee6452251
Vi Doan <viidoan2505@gmail.com>
2022-08-05 00:51:19 +0700

    Updates

---
+++ precache-manifest.550e9893d9566a98793a1ca034aa3068.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "12b153ae64f70c081617",
+    "url": "/findRobo/static/css/main.d035b89d.chunk.css"
+  },
+  {
+    "revision": "12b153ae64f70c081617",
+    "url": "/findRobo/static/js/main.12b153ae.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/findRobo/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "1c58326ff6531bbcec87",
+    "url": "/findRobo/static/css/2.ca53ed27.chunk.css"
+  },
+  {
+    "revision": "1c58326ff6531bbcec87",
+    "url": "/findRobo/static/js/2.1c58326f.chunk.js"
+  },
+  {
+    "revision": "9b0071489ce236d0494a",
+    "url": "/findRobo/static/js/runtime~main.9b007148.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/findRobo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0956b16fae8d9da6b0e64d3b882ef672",
+    "url": "/findRobo/index.html"
+  }
+];
